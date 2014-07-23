bbd66dbd62bee0bdff46f414c73e8885f1473d6a
t-usui <alc.fetch@gmail.com>
2014-07-24 01:59:46 +0900

    Implemented initial functions of ida_asm_generator.py

--- src/ida_asm_generator.py
+++ src/ida_asm_generator.py
@@ -2,20 +2,37 @@
 #-*- coding:utf-8 -*-
 
 import commands
+import shutil
+import os
 
 class IDAAsmGenerator(object):
     
     def __init__(self):
-        self.ida_executable = '/Applications/idaq.app/Contents/MacOS/idaq'
-    
+        self.ida_executable = '/cygdrive/c/Program\ Files/IDA\ Free/idag.exe'
+        self.exe_path = '../exe'
+        self.asm_path = '../asm'
+        
     def __del__(self):
         pass
     
     def generate(self, file_name):
-        generate_command = self.ida_executable + ' -B ' + file_name
+        self.generate_asm(file_name)
+        self.remove_idb(file_name)
+        self.move_asm(file_name)
+    
+    def generate_asm(self, file_name):
+        generate_command = self.ida_executable + ' -B ' + self.exe_path + os.sep + file_name        
+        commands.getoutput(generate_command)
+        
+    def remove_idb(self, file_name):
+        idb_file_name = file_name.split('.')[0] + '.idb'
+        os.remove(self.exe_path + os.sep + idb_file_name)
+        
+    def move_asm(self, file_name):
+        asm_file_name = file_name.split('.')[0] + '.asm'
+        shutil.move('.' + os.sep + asm_file_name, self.asm_path + os.sep + asm_file_name)
         
 if __name__ == '__main__':
     generator = IDAAsmGenerator()
     
-    generator.generate('/Users/isc/Documents/Programs/Research/compiler_estimater/signature_matcher/assiral.exe')
-        
+    generator.generate('Assiral_bcc.exe')
